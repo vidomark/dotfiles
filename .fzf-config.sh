8ad6477630a739455515470a2aa4ece2ce355e78
mark.vido <mark.vido@intuitech.studio>
2024-05-05 13:34:24 +0200

    fzf colorscheme change

--- .fzf-config.sh
+++ .fzf-config.sh
@@ -29,22 +29,22 @@ _fzf_comprun() {
 
 _gen_fzf_default_opts() {
 
-local color00='#1d2021'
-local color01='#3c3836'
+local color00='#202020'
+local color01='#2a2827'
 local color02='#504945'
-local color03='#665c54'
+local color03='#5a524c'
 local color04='#bdae93'
-local color05='#d5c4a1'
+local color05='#ddc7a1'
 local color06='#ebdbb2'
 local color07='#fbf1c7'
-local color08='#fb4934'
-local color09='#fe8019'
-local color0A='#fabd2f'
-local color0B='#b8bb26'
-local color0C='#8ec07c'
-local color0D='#83a598'
+local color08='#ea6962'
+local color09='#e78a4e'
+local color0A='#d8a657'
+local color0B='#a9b665'
+local color0C='#89b482'
+local color0D='#7daea3'
 local color0E='#d3869b'
-local color0F='#d65d0e'
+local color0F='#bd6f3e'
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
 " --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
