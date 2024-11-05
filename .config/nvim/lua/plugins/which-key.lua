2727c112b4a59c9e2991a361d661ff9c7cbb6714
mark.vido <mark.vido@intuitech.studio>
2024-11-05 16:30:43 +0100

    which-key update

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,11 +2,16 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   init = function()
-    local wk = require("which-key")
-    wk.register({
-      r = { name = "refactor" },
-      e = { name = "diagnostics" },
-      v = { name = "views" },
-    }, { prefix = "<leader>" })
+    require("which-key").add({
+      { "<leader>r", group = "refactor" },
+      { "<leader>e", group = "diagnostics" },
+      { "<leader>v", group = "views" },
+    })
+
+    -- wk.register({
+    --   r = { name = "refactor" },
+    --   e = { name = "diagnostics" },
+    --   v = { name = "views" },
+    -- }, { prefix = "<leader>" })
   end,
 }
